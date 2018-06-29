fe72ae6714bc6b11729ec9b83c892d8f5a71e647
xiaohesong <didmehh@163.com>
2018-06-29 15:36:21 +0800

    :date: code splitting not work

--- src/App.js
+++ src/App.js
@@ -1,116 +1,37 @@
 import React, { Component } from 'react';
-import SplitPane from 'react-split-pane';
-import ReactMarkdown from 'react-markdown';
-import assign from 'lodash.assign';
-import Editor from './editor.js';
-import CodeBlock from './code-block.js';
-import Toolbar from './toolbar.js';
-
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
-
 import dyanc from './dyanc'
-import Homer from './Home'
-require('highlight.js/styles/monokai.css');
 
-const fs = window.require('fs');
-const isDev = window.require('electron-is-dev');
-const electron = window.require('electron');
-const path = window.require('path');
-const electron_process = electron.process;
-const electron_app = electron.app;
-const ipc = electron.ipcRenderer;
-const remote = electron.remote;
-const dialog = remote.dialog;
+const chokidar = window.require('chokidar')
 
+const Md = dyanc(() => import('./Md'))
 const Home = dyanc(() => import('./Home'))
 
 class App extends Component {
-  constructor(props) {
-    super();
+  componentDidMount() {
 
-    ipc.on('file-opened', (event, file, content) => {
-      this.setState({
-        markdownSrc: content
-      });
+    const watcher = chokidar.watch('/Users/xiaohesong/workspace/my-files', {
+      ignored: /(^|[\/\\])\../,
+      persistent: true,
+      ignoreInitial: true
     });
 
-    ipc.on('save-file', (event) => {
-      dialog.showSaveDialog((filename) => {
-        if(filename == undefined){
-          return;
-        }
-
-        var content = this.state.markdownSrc;
-
-        fs.writeFile(filename, content, (err) => {
-          if (err) console.log(err);
-          alert("The file has been successfully saved.");
-        })
-      });
+    watcher.on('all', (event, path) => {
+      console.log(`File ${path}, event is ${event}`);
     });
-
-    // Open the readme file
-    const file_name = path.join((isDev ? '' : window.process.resourcesPath), "public/welcome.md");
-    const content = fs.readFileSync(file_name).toString();
-
-    this.state = {
-      markdownSrc: content,
-      splitPaneSize: "50%",
-      homeable: false
-    };
-
-    this.onMarkdownChange = this.onMarkdownChange.bind(this);
-    this.onViewChange = this.onViewChange.bind(this);
-  }
-
-  onMarkdownChange(md) {
-    this.setState({
-      markdownSrc: md
-    });
-  }
-
-  onViewChange(e) {
-    switch(e) {
-      case "editor":
-        this.setState({ splitPaneSize: "100%"})
-        break;
-      case "split":
-        this.setState({ splitPaneSize: "50%"})
-        break;
-      case "view":
-        this.setState({ splitPaneSize: "0%"})
-        break;
-      default:
-        break;
-    }
-  }
-
-  handleClick = () => {
-    this.setState({homeable: !this.state.homeable})
   }
 
   render() {
-    const Show = this.state.homeable ? <Home/> : null
-    console.log('showable is', this.state.homeable, Show);
     return (
-      <div className="App">
-        <Toolbar onClick={this.onViewChange}/>
-        {Show}
-        <Homer />
-        <button onClick={this.handleClick}>name is xiaohesong</button>
-        <SplitPane split="vertical" size={this.state.splitPaneSize}>
-          <div className="editor-pane">
-            <Editor className="editor" value={this.state.markdownSrc} onChange={this.onMarkdownChange}/>
-          </div>
-          <div className="view-pane">
-            <ReactMarkdown className="result"
-              source={this.state.markdownSrc}
-              renderers={assign({}, ReactMarkdown.renderers, {CodeBlock: CodeBlock})}
-            />
-          </div>
-        </SplitPane>
-      </div>
+      <Router>
+        <div className="App">
+          <Link to='/home'>Home页面</Link>
+          <Link to='/md'>Markdown</Link>
+          <Route component={Home} path='/home' />
+          <Route component={Md} path='/md'/>
+        </div>
+      </Router>
     );
   }
 }
