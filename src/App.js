aae615c1fb311a187da617525da3c5dc2b0a6298
xiaohesong <didmehh@163.com>
2018-06-29 17:35:30 +0800

    read file async

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 import dyanc from './dyanc'
 
 const chokidar = window.require('chokidar')
+const fs = window.require('fs')
 
 const Md = dyanc(() => import('./Md'))
 const Home = dyanc(() => import('./Home'))
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    const content = fs.readFileSync('/Users/xiaohesong/workspace/my-files/xx.md', 'utf-8')
+    console.log("content uis", content);
+  }
+
   componentDidMount() {
 
     const watcher = chokidar.watch('/Users/xiaohesong/workspace/my-files', {
